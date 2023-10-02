e6e3a459f0f519174f7baab6536a84e2f3d8926c
julieniut <92865222+julieniut@users.noreply.github.com>
2023-10-02 18:59:07 +0200

    AJOUTE de l'heure

--- calendrier.py
+++ calendrier.py
@@ -5,27 +5,27 @@ from datetime import datetime
 class DateSelectionWidget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.setWindowTitle("Sélection de date")
+        self.setWindowTitle("Sélection de date et heure")
 
         layout = QVBoxLayout(self)
 
-        self.date_var = QDateTimeEdit(self)
-        self.date_var.setCalendarPopup(True)
-        self.date_var.setDateTime(datetime.today())
-        layout.addWidget(self.date_var)
+        self.date_time_var = QDateTimeEdit(self)
+        self.date_time_var.setCalendarPopup(True)
+        self.date_time_var.setDateTime(datetime.today())
+        layout.addWidget(self.date_time_var)
 
-        self.valider_button = QPushButton("Valider la date", self)
+        self.valider_button = QPushButton("Valider la date et l'heure", self)
         self.valider_button.clicked.connect(self.valider_date)
         layout.addWidget(self.valider_button)
 
     def valider_date(self):
-        selected_date = self.date_var.dateTime()
-        formatted_date = selected_date.toString("dd/MM/yyyy")
-        print("Date sélectionnée :", formatted_date)
+        selected_date_time = self.date_time_var.dateTime()
+        formatted_date_time = selected_date_time.toString("dd/MM/yyyy HH:mm:ss")
+        print("Date et heure sélectionnées :", formatted_date_time)
 
 app = QApplication(sys.argv)
 fenetre = QMainWindow()
-fenetre.setWindowTitle("Sélection de date")
+fenetre.setWindowTitle("Sélection de date et heure")
 
 date_selection_widget = DateSelectionWidget()
 fenetre.setCentralWidget(date_selection_widget)
