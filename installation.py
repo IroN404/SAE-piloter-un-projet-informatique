2ab81b391f58a99975b5b524fb670fecdf97f86a
julieniut <92865222+julieniut@users.noreply.github.com>
2023-10-03 11:35:32 +0200

    Installation.py V1.0 Julien

---
+++ installation.py
@@ -0,0 +1,31 @@
+import ast
+import subprocess
+
+# Remplacez le chemin vers votre script Python ici
+script_path = 'main.py'
+
+# Lire le contenu du script
+with open(script_path, 'r') as file:
+    code = file.read()
+
+# Analyser le code source pour extraire les noms des modules importés
+tree = ast.parse(code)
+imports = []
+
+for node in ast.walk(tree):
+    if isinstance(node, ast.Import):
+        for name in node.names:
+            imports.append(name.name)
+    elif isinstance(node, ast.ImportFrom):
+        module = node.module
+        for name in node.names:
+            imports.append(f"{module}.{name.name}")
+
+# Installer les modules importés via pip
+for module in imports:
+    try:
+        subprocess.check_call(['pip', 'install', module])
+    except subprocess.CalledProcessError:
+        print(f"Impossible d'installer le module {module}")
+
+print("Installation des dépendances terminée.")
