73faf3e7e3f9f8c3a8b45c608009949bce7fe3b1
Bahir <bahirboudouma2003@gmail.com>
2023-10-04 16:24:19 +0200

    v1 bahir 23

--- installation.py
+++ installation.py
@@ -1,35 +1,17 @@
 import ast
 import subprocess
 
-#mise a jour de pip
-subprocess.check_call('pip3 install --upgrade pip')
-
-# script Python
-script_path = 'main.py'
-
-# Lire le contenu du script
-with open(script_path, 'r') as file:
-    code = file.read()
-
-# Analyser le code source pour extraire les noms des modules importés
-tree = ast.parse(code)
-imports = []
+# Installation de tous les paquets nécessaires dans le code main
+print("Installation des dépendances...")
+with open("main.py", "r") as f:
+    for line in f.readlines():
+        if line.startswith("import"):
+            subprocess.run(["pip", "install", line.split()[1]])
+        elif line.startswith("from"):
+            subprocess.run(["pip", "install", line.split()[1]])
+        else:
+            break
 
 
-for node in ast.walk(tree):
-    if isinstance(node, ast.Import):
-        for name in node.names:
-            imports.append(name.name)
-    elif isinstance(node, ast.ImportFrom):
-        module = node.module
-        for name in node.names:
-            imports.append(f"{module}.{name.name}")
-
-# Installer les modules importés via pip
-for module in imports:
-    try:
-        subprocess.check_call(['pip3', 'install', module])
-    except subprocess.CalledProcessError:
-        print(f"Impossible d'installer le module {module}")
 
 print("Installation des dépendances terminée.")
