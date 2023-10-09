72ce6771080cf0897e51b743ecc067f3f06a762b
Grégory Maitre <95073184+IroN404@users.noreply.github.com>
2023-10-09 14:34:57 +0200

    Squellette de l'app

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import flet as f
 
 def main(page: f.Page):
-    page.add(f.Text(value="Starting Page"))
+    ## Page Champs de saisie
+    # Champ de saisie nom de la tache
+    # Champ de saisie nom de la personne
+    # Menu dropdown importance de la tache
+    # Menu dropdown etiquette
+    # Menu selection de la date
+
+    ## Page Liste des taches
+    # Tache
+    # Coche tache effectuée
+
+    ## Page principale
+    # ALL
+    page.add(f.InputBorder(value="Enter a Name"))
 f.app(target=main)
 
