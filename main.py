cdf04bc44c8f769d5b4a677d440e6f9967317971
julieniut <julien.losser@uha.fr>
2023-10-09 15:57:16 +0200

    nom de la personne ajouter a côté de la tache

--- main.py
+++ main.py
@@ -14,21 +14,21 @@ def main(page: f.Page):
 
     # Bouton ajouter la tache
     def add_clicked(e):
-        if new_task.value == "":
-            open_dlg(e)  # Ouvre la boîte de dialogue si le champ est vide
+        if new_task.value == "" or person_name.value == "":
+            open_dlg(e)  # Ouvre la boîte de dialogue si le champ de tâche ou de nom de personne est vide
         else:
-            # Ajoute une case à cocher avec le texte entré dans le champ
-            page.add(f.Checkbox(label=new_task.value))
-            new_task.value = ""  # Réinitialise le champ de saisie
+            # Ajoute une case à cocher avec le texte de la tâche et le nom de la personne
+            task_with_person = f.Checkbox(label=f"{new_task.value} - {person_name.value}")
+            page.add(task_with_person)
+            new_task.value = ""  # Réinitialise le champ de tâche
+            person_name.value = ""  # Réinitialise le champ de nom de personne
             page.update()
 
     # Champ de saisie nom de la tache
     new_task = f.TextField(hint_text="Nouvelle tâche")
     page.add(new_task, f.FloatingActionButton(icon=f.icons.ADD, on_click=add_clicked))
 
-
-
-    # Champ de saisie nom de la personne (commentaire manquant)
+    # Champ de saisie nom de la personne
     person_name = f.TextField(hint_text="Nom de la personne")
     page.add(person_name)
 
