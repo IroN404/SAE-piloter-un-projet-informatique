112c64e714b751143b6f4d56fc4941509f1c3450
julieniut <julien.losser@uha.fr>
2023-10-12 09:05:48 +0200

    priorité validé si l'entrée est correcte

--- main.py
+++ main.py
@@ -7,10 +7,19 @@ def main(page: f.Page):
         title=f.Text("Entrez un nom pour la tâche")
     )
 
+    InvalidPriority = f.AlertDialog(
+        title=f.Text("Priorité invalide")
+    )
+
     # Fonction pour ouvrir la boîte de dialogue
     def open_dlg(e):
         page.dialog = EmptyTaskName
         EmptyTaskName.open = True
+        page.update()
+
+    def open_invalid_priority_dlg(e):
+        page.dialog = InvalidPriority
+        InvalidPriority.open = True
         page.update()
 
     # Bouton ajouter la tache
@@ -26,15 +35,15 @@ def main(page: f.Page):
 
             # Vérifiez si la priorité saisie par l'utilisateur est valide
             if user_priority not in allowed_priorities:
-                user_priority = ""  # Réinitialise la priorité si elle n'est pas valide
-
-            # Ajoute une case à cocher avec le texte de la tâche et la priorité
-            task_with_info = f.Checkbox(label=f"{new_task.value} - {person_name.value} - Priorité : {user_priority}")
-            page.add(task_with_info)
-            new_task.value = ""  # Réinitialise le champ de tâche
-            person_name.value = ""  # Réinitialise le champ de nom de personne
-            priorite.value = ""  # Réinitialise le champ de priorité
-            page.update()
+                open_invalid_priority_dlg(e)  # Ouvre la boîte de dialogue si la priorité n'est pas valide
+            else:
+                # Ajoute une case à cocher avec le texte de la tâche et la priorité
+                task_with_info = f.Checkbox(label=f"{new_task.value} - {person_name.value} - Priorité : {user_priority}")
+                page.add(task_with_info)
+                new_task.value = ""  # Réinitialise le champ de tâche
+                person_name.value = ""  # Réinitialise le champ de nom de personne
+                priorite.value = ""  # Réinitialise le champ de priorité
+                page.update()
 
     # Champ de saisie nom de la tache
     new_task = f.TextField(hint_text="Nouvelle tâche")
