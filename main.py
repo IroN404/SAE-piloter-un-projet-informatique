4c1100db5baca39e1ff9f8a8623e2fb4fbcdbaa2
Grégory Maitre <95073184+IroN404@users.noreply.github.com>
2023-10-31 10:39:22 +0100

    Restructuring from flet framework to pyqt due to limitations

--- main.py
+++ main.py
@@ -1,110 +1,40 @@
-import flet as f
-import flet as ft
+from PyQt5.QtWidgets import *
+import sys
 
+#from assets.login import LoginWidget
+#from assets.landing import LandingWidget
+#from assets.signup import SignupWidget
 
-def main(page: f.Page):
-    img = f.Image(
-        src=f"/Users/gregory/PycharmProjects/SAE-piloter-un-projet-informatique/media/logo_night.png",
-        height=200,
-        width=200,
-        fit=f.ImageFit.CONTAIN,
-    )
-    ## Appbar
-    page.appbar = f.AppBar(
-        leading_width=200,
-        title=f.Image(f"https://github.com/IroN404/SAE-piloter-un-projet-informatique/blob/main/media/logo_day.png?raw=true",
-                      height=95),
-        toolbar_height=70,
-        center_title=True,
-        bgcolor=f.colors.SURFACE_VARIANT,
-        actions=[
-            f.IconButton(f.icons.WB_SUNNY_OUTLINED)
-        ],
-    )
-    ## Page Champs de saisie
-    # Alert Dialog
-    EmptyTaskName = f.AlertDialog(
-        title=f.Text("Entrez un nom pour la tâche")
-    )
+app = QApplication([])
 
+def main():
+    # Main window properties
+    main_window = QMainWindow() # creation de la fenetre
+    main_window.setWindowTitle("GUI-todo-app") # titre de la fenetre
+    main_window.setContentsMargins(0, 0, 0, 0) # remove margins
+    main_window.setFixedSize(800, 600) # window's size
+    main_window.setStyleSheet("style/main_window.css") # window's style
 
+    # QstackedWidget creation
+    stacked_widget = QStackedWidget()
+    main_window.setCentralWidget(stacked_widget)
 
-    def open_dlg(e):
-        page.dialog = EmptyTaskName
-        EmptyTaskName.open = True
-        page.update()
-    # Bouton ajouter la tache
-    def add_clicked(e):
-        if new_task.value==(""):
-            open_dlg(e)
-        else:
-            page.add(f.Checkbox(label=f'{new_task.value}  {priority_selector.value}  {tags_selector.value}'))
-            new_task.value = ("")
-            page.update()
+    # Listing all pages of the app
+    #login_page = LoginWidget(stacked_widget)
+    #landing_page = LandingWidget(stacked_widget)
+    #signup_page = SignupWidget(stacked_widget)
 
+    # Adding pages to the QStackedWidget
+    #stacked_widget.addWidget(login_page)
+    #stacked_widget.addWidget(landing_page)
+    #stacked_widget.addWidget(signup_page)
 
+    # Displaying the login page
+    #stacked_widget.setCurrentWidget(login_page)
 
-    # Champ de saisie nom de la tache
-    new_task = f.TextField(hint_text="Nouvelle tâche",label='nom de la tache', expand=True)
-    # Champ de saisie nom de la personne
-    person = f.TextField(hint_text="Nom de la personne",label='nom', expand=True)
-    # Menu dropdown importance de la tache
-    priority_selector = f.Dropdown(
-        width=200,
-        label="Priorité",
-        hint_text="Niveau de priorité",
-        options=[
-            f.dropdown.Option("Urgent"),
-            f.dropdown.Option("Important"),
-            f.dropdown.Option("Normal"),
-        ],
-    )
-    # Menu dropdown etiquette
-    tags_selector = f.Dropdown(
-        width=200,
-        label="Etiquette",
-        hint_text="Choisissez une étiquette",
-        options=[
-            f.dropdown.Option("Travail"),
-            f.dropdown.Option("Ecole"),
-            f.dropdown.Option("Activitée"),
-            f.dropdown.Option("Autre"),
-        ],
-    )
-    # Menu selection de la date
+    # Starting the app
+    main_window.show()
+    sys.exit(app.exec_())
 
-
-    ## Page Liste des taches
-    tasks_view = f.Column()
-    # Tache
-    # Coche tache effectuée
-
-    ## Page principale
-    # ALLs
-
-
-
-    view = f.Column(
-        width=600,
-        controls=[
-            f.Row(
-                controls=[
-                    new_task,
-                ],
-            ),
-            f.Row(
-                controls=[
-                    person,
-                    priority_selector,
-                    tags_selector,
-                    f.FloatingActionButton(icon=f.icons.ADD, on_click=add_clicked),
-                ],
-            ),
-            tasks_view,
-        ],
-    )
-    page.horizontal_alignment = f.CrossAxisAlignment.CENTER
-    page.add(view)
-
-f.app(target=main)
-
+if __name__ == "__main__":
+    main()
