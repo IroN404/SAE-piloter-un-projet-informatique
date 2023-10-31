eacda6d130cbacf5dab24c219a05d3329315f692
Grégory Maitre <95073184+IroN404@users.noreply.github.com>
2023-10-31 11:12:20 +0100

    restructuring functions

--- main.py
+++ main.py
@@ -5,16 +5,14 @@ import sys
 #from assets.landing import LandingWidget
 #from assets.signup import SignupWidget
 
-app = QApplication([])
-
-def main():
+def initUI(main_window):
     # Main window properties
-    main_window = QMainWindow() # creation de la fenetre
     main_window.setWindowTitle("GUI-todo-app") # titre de la fenetre
     main_window.setContentsMargins(0, 0, 0, 0) # remove margins
     main_window.setFixedSize(800, 600) # window's size
     main_window.setStyleSheet("style/main_window.css") # window's style
 
+def StackeWidget(main_window):
     # QstackedWidget creation
     stacked_widget = QStackedWidget()
     main_window.setCentralWidget(stacked_widget)
@@ -31,6 +29,13 @@ def main():
 
     # Displaying the login page
     #stacked_widget.setCurrentWidget(login_page)
+
+def main():
+    app = QApplication([])
+
+    main_window = QMainWindow()
+    initUI(main_window)
+    StackeWidget(main_window)
 
     # Starting the app
     main_window.show()
